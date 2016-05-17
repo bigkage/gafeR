2fe458fcafd740ebe2d15ca9a34460e8ea9a13a7
kmkmjohnson <kmkmjohnson@gmail.com>
2016-05-17 14:50:11 -0700

    Added multiple gets and pushed for users, groups, org units, and chrome devices.

--- gafeR.R
+++ gafeR.R
@@ -11,9 +11,13 @@ options(
   "googleAuthR.scopes.selected" = c(
     "https://www.googleapis.com/auth/classroom.courses",
     "https://www.googleapis.com/auth/admin.directory.user",
-    "https://www.googleapis.com/auth/admin.directory.device.chromeos"
+    "https://www.googleapis.com/auth/admin.directory.device.chromeos",
+    "https://www.googleapis.com/auth/admin.directory.group",
+    "https://www.googleapis.com/auth/urlshortener",
+    "https://www.googleapis.com/auth/admin.directory.orgunit"
   )
 )
+options(googleAuthR.rawResponse = FALSE)
 
 ##### Users #####
 # return dataframe with all user accounts in the domain
@@ -73,6 +77,132 @@ get_users <- function(domain) {
   }
 }
 
+#https://www.googleapis.com/admin/directory/v1/users
+# force password reset on first login
+add_user <-
+  function(family_name,
+           given_name,
+           primary_email,
+           temp_password) {
+    f <-
+      gar_api_generator(
+        "https://www.googleapis.com/admin/directory/v1",
+        "POST",
+        path_args = list(users = ""),
+        data_parse_function = function(x)
+          x$id
+      )
+    
+    body <-
+      list(
+        name = list(familyName = family_name, givenName = given_name),
+        password = temp_password,
+        primaryEmail = primary_email,
+        changePasswordAtNextLogin = TRUE
+      )
+    
+    # body_json <- toJSON(body)
+    
+    f(the_body = body)
+  }
+
+
+##### OrgUnits #####
+
+get_org_units <- function(customer_ID, org_path) {
+  f <-
+    gar_api_generator(
+      "https://www.googleapis.com/admin/directory/v1",
+      "GET",
+      path_args = list(customer = "default_customer_ID", orgunits = ""),
+      pars_args = list(type = "all")
+    )
+  
+  g_org_units <-
+    f(the_body = body,
+      path_arguments = list(customer = customer_ID))
+  
+  return(as.data.frame(g_org_units$content$organizationUnits))
+}
+
+
+add_org_unit <- function(customer_ID, org_name, parent_path) {
+  #https://www.googleapis.com/admin/directory/v1/customer/{customerId}/orgunits
+  
+  body = list(name = org_name,
+              parentOrgUnitPath = parent_path)
+  
+  f <-
+    gar_api_generator(
+      "https://www.googleapis.com/admin/directory/v1",
+      "POST",
+      path_args = list(customer = "default_customer_ID", orgunits = ""),
+      data_parse_function = function(x)
+        x$id
+    )
+  
+  f(the_body = body,
+    path_arguments = list(customer = customer_ID))
+  
+}
+
+
+##### Groups #####
+# need to add looping for domains with more than 500 groups
+# maybe add optional parameter to return one group based on email address
+
+get_groups <- function(domain) {
+  f <-
+    gar_api_generator(
+      "https://www.googleapis.com/admin/directory/v1/groups",
+      "GET",
+      pars_args = list(domain = domain,
+                       maxResults = "500")
+    )
+  
+  g_groups <- f(the_body = body)
+  
+  return(as.data.frame(g_groups$content$groups))
+  
+}
+
+
+# get all members of a given group
+# need to add looping to accomodate groups with more than 500 members
+get_members <- function(group_key) {
+  
+  f <-
+    gar_api_generator(
+      "https://www.googleapis.com/admin/directory/v1",
+      "GET",
+      pars_args = list(maxResults = "500"),
+      path_args = list(groupKey = "default_group_key")
+    )
+  
+  g_group_members <-
+    f(the_body = body,
+      path_arguments = list(groupKey = group_key))
+  
+  return(as.data.frame(g_group_members$content$members))
+}
+
+
+add_member <- function(email_address, group_email){
+  
+  # API https://www.googleapis.com/admin/directory/v1/groups/{groupKey}/members
+  
+  body = list(
+    email = email_address
+  )
+  
+  f <- gar_api_generator("https://www.googleapis.com/admin/directory/v1",
+                         "POST",
+                         path_args = list(groups = "defaultGroupKey", members = ""),
+                         data_parse_function = function(x) x$id)
+  
+  f(the_body = body, path_arguments = list(groups = group_email))
+  
+}
 
 ##### Classroom #####
 # may need to add loop to handle multiple pages of returned classes
@@ -90,26 +220,21 @@ get_classes <- function() {
 
 ##### ChromeOS Devices #####
 # https://developers.google.com/admin-sdk/directory/v1/reference/chromeosdevices
-# need to create loop, current default is 100 returned records at a time
+# fix path arguments
 
-get_chrome_devices <- function(customerID) {
+get_chrome_devices <- function(customer_ID) {
   page_token <- NULL # used to track next page
-  
-  # construct API URL to include customerID which is found via user query
-  # may want to add a way to automatically find the customerID via another query
-  apiURL <-
-    paste(
-      "https://www.googleapis.com/admin/directory/v1/customer/",
-      customerID,
-      "/devices/chromeos",
-      sep = ""
-    )
   
   repeat {
     # construct and process first time api called
     if (is.null(page_token)) {
-      f <- gar_api_generator(apiURL, "GET",
-                             pars_args = list(maxResults = "500"))
+      f <-
+        gar_api_generator(
+          "https://www.googleapis.com/admin/directory/v1/customer/{customerID}/devices/chromeo",
+          "GET",
+          pars_args = list(maxResults = "500"),
+          path_args = list(customerID = customer_ID)
+        )
       
       g_devices <- f(the_body = body)
       
